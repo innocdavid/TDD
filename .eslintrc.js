5629d494df15d53d6b26df39735d6917c28962cc
kali <innocdavid@gmail.com>
2022-05-23 09:20:47 -0400

    ADDING ESLINT AND MODULE DIRECTORY -added .eslint directory -added /node_module directory -added package_lock.json file

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  env: {
+    es2021: true,
+    mocha: true,
+    chai: true
+  },
+  extends: [
+    'airbnb-base',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: [
+    '@typescript-eslint',
+  ],
+  rules: {
+  },
+};
